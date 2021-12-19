2b4339f0b2ee266169a9d0311da43e601f9dee62
bimashazaman <bimashazaman1234@gmail.com>
2021-12-19 06:47:08 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css'
 import Header from './components/Header'
 import Home from './components/Home'
+import CheckOut from './components/CheckOut'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<CheckOut />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/CheckOut.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CheckOut = () => {
+  return <div></div>
+}
+
+export default CheckOut
